48d7090e495d59dc93826cf30f9335b8cdcc0e70
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-10 14:59:55 +0200

    Serve the client side through the Flask app (now it works)
    Backbone was not triggering the '/' route because it is very sensitive
    to the URL path component. When serving directly from the file system,
    this results in Backbone drawing incorrect conclusions about the route.
    
    As it turns out, the client side part was already correct.

--- campbellsoup/api.py
+++ campbellsoup/api.py
@@ -9,9 +9,9 @@ import flask
 from .models import *
 from .queries import *
 
-api = flask.Blueprint('API', __name__)
+api = flask.Blueprint('API', __name__, static_url_path='', static_folder='../.tmp')
 
 
 @api.route('/')
 def index():
-    return 'Hello, World!'
+    return flask.send_from_directory(api.static_folder, 'index.html')
