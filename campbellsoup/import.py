5ced6c9a0b241c042d9a1d8e71a52e49fa57b95f
Julian Gonggrijp <j.gonggrijp@uu.nl>
2017-04-07 13:18:21 +0200

    Continue implementing the import command (#29)
    Open challenge: author attribution. Currently, the data model only
    permits associations between persons and groups through the Revision
    model, with only a single author per revision. We may need more
    flexibility.

--- campbellsoup/import.py
+++ campbellsoup/import.py
@@ -3,12 +3,17 @@
 import sys
 import os
 import os.path as op
+import logging
 from datetime import date
 
 from flask_script import Manager
 
-from .parsers import document, filename_parts
+from .parsers import document, latex_writer_sources, filename_parts
 import .models as m
+
+MY_NAME = 'auto_import'
+
+logger = logging.getLogger(__name__)
 
 
 def process_options(app, **kwargs):
@@ -22,7 +27,24 @@ import_manager = Manager(process_options)
 
 @import_manager.command
 def import_test(directory, title=None, year=None, order_by_stdin=False):
-    """ Import all question group and image files in `directory`. """
+    """
+    Import question group and image files from `directory` nonrecursively.
+    
+    Creates a new Test object to which the imported question groups will
+    be associated. `title` is used as the name for the new test and `year`
+    is used for its date.
+    If not provided, `title` and `year` will be derived from the last
+    path segment of `directory`.
+    
+    All files from `directory` are imported in numerical-alphabetical
+    order, unless `order_by_stdin` is set. In that case, pass the names of
+    the files to be imported on the standard input, one file per line. The
+    names should include the file extension but exclude the leading path
+    component, which is already provided through `directory`. If the
+    manual listing includes figures, group the figures together with the
+    text files they belong to, with the text file first, and list the
+    figure files in the same order in which they appear in the text file.
+    """
     if year is None:
         year = op.split(op.normpath(path))[1]
     if title is None:
@@ -51,10 +73,10 @@ def import_test(directory, title=None, year=None, order_by_stdin=False):
 def get_auto_import_person(session):
     """ Return the auto_import `Person`. Create if it does not exist. """
     me = session.query(m.Person).filter_by(
-        short_name='auto_import',
+        short_name=MY_NAME,
     ).one_or_none()
     if me is None:
-        me = m.Person(short_name='auto_import', full_name='Automated Import')
+        me = m.Person(short_name=MY_NAME, full_name='Automated Import')
         session.add(me)
     return me
 
@@ -63,16 +85,15 @@ def import_groups(files, test, revision, session):
     """ Import `files` in the given order and add to `test`. """
     order = 1
     blocks = []   # blocks that mention figures
-    figures = []  # that will fill the gaps in `blocks`
+    figures = []  # figures that will fill the gaps in `blocks`
     for filename in files:
         if op.splitext(filename)[1] == '.txt':
+            # We first import a group and then any figures belonging to it.
+            # So we bind the groups and figures together before starting a new
+            # group.
             session.add_all(bind_figures(blocks, figures))
             blocks, figures = [], []
-            try:
-                text = open(filename).read()
-            except ValueError:  # Silly Windows file
-                text = open(filename, encoding='cp1252').read()
-            group, blocks = import_textfile(text, revision, session)
+            group, blocks = import_textfile(filename, revision, session)
             session.add(m.TestGroupBinding(test=test, group=group, order=order))
             order += 1
         else:
@@ -80,8 +101,59 @@ def import_groups(files, test, revision, session):
     session.add_all(bind_figures(blocks, figures))
 
 
-def import_textfile(text, revision, session):
+def import_textfile(filename, revision, session):
     """ Parse and import the contents of an archive textfile. """
+    logger.info('Importing textfile {}'.format(filename))
+    try:
+        text = open(filename).read()
+    except ValueError:  # Silly Windows file
+        text = open(filename, encoding='cp1252').read()
+    tree = document.parseString(text, True)
+    if 'contentPlain' in tree:
+        return import_plain(tree, revision, session)
+    elif 'contentLW' in tree:
+        raw_blocks = latex_writer_sources.parseString(text, True)
+        return import_latex_writer(tree, raw_blocks, revision, session)
+    else:
+        raise KeyError('No known content type in parse tree')
+
+
+def import_plain(tree, revision, session):
+    """ Import a group and its blocks from parsing `tree` in plain notation. """
+    group = m.Group(
+        revision=revision,
+        format=get_plain_format(),
+        network=get_group_network(tree.get('reuse')),
+        title=tree.get('title'),
+    )
+    block_count = len(tree['contentPlain'])
+    if 'questionCount' in tree:
+        question_count = tree['questionCount'][0]
+    else:
+        question_count = block_count
+    intro_count = 0
+    if block_count < question_count:
+        logger.warning('{} questions claimed but only {} blocks found'.format(
+            question_count,
+            block_count,
+        ))
+    elif block_count > question_count:
+        intro_count = block_count - question_count
+    
+
+
+def import_latex_writer(tree, sources, revision, session):
+    """ Import a group and its blocks from `tree` with `sources` in LaTeX-w. """
+    pass
+
+
+def import_figure(filename, revision, session):
+    """ Import an image file as an m.Figure. """
+    pass
+
+
+def bind_figures(blocks, figures):
+    """ Bind imported figures to blocks that claim to need figures. """
     pass
 
 
