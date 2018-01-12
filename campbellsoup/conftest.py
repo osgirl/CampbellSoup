c7cd539ab6b017e26bc1ff4a98ad7e28a10938ca
Julian Gonggrijp <j.gonggrijp@uu.nl>
2018-01-12 16:47:14 +0100

    Add unittests for the account password business

--- campbellsoup/conftest.py
+++ campbellsoup/conftest.py
@@ -1,8 +1,29 @@
-# (c) 2017 Julian Gonggrijp
+# (c) 2017, 2018 Julian Gonggrijp
+
+from random import Random
+from string import printable
 
 import pytest
 
 from .parsers import *
+
+
+def generate_random_passwords(count=None, mean_length=12, seed=None):
+    """ Generate `count` random passwords, or infinitely many if None. """
+    prng = Random(seed)
+    labda = 1 / mean_length
+    if count is None:
+        count = -1
+    while count != 0:
+        count -= 1
+        length = round(prng.expovariate(labda))
+        yield ''.join((prng.choice(printable) for i in range(length)))
+
+
+@pytest.fixture(params=generate_random_passwords(15))
+def random_password_fix(request):
+    return request.param
+
 
 # The examples below are based on real question files. They were
 # bananafied in order to keep confidential information confidential.
---
+++ campbellsoup/models_test.py
@@ -0,0 +1,36 @@
+# (c) 2018 Julian Gonggrijp
+
+import pytest
+
+from .models import Account, Person
+
+
+@pytest.fixture
+def account_password_fix():
+    person = Person(short_name='test', full_name='test test')
+    account = Account(person=person)
+    password = 'banana'
+    return person, account, password
+
+
+def test_Account_password_storage(account_password_fix):
+    person, account, password = account_password_fix
+    assert account.password_hash is None
+    with pytest.raises(AttributeError):
+        assert account.password == password
+    account.password = password
+    assert account.password_hash is not None
+    assert account.password_hash != ''
+    assert account.password_hash != password
+    with pytest.raises(AttributeError):
+        assert account.password == password
+    assert account.verify_password(password)
+
+
+def test_Account_password_invalid(account_password_fix, random_password_fix):
+    # This is a fuzz test: every time pytest runs, 15 different
+    # `random_password_fix`es are tried against this test. Eventually,
+    # all possible passwords will have been tested. ;-)
+    person, account, password = account_password_fix
+    account.password = password
+    assert not account.verify_password(random_password_fix)
