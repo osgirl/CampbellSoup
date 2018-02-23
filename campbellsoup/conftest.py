bb81df9246a5e4aeca9a3e2ec5ed949cdea0e1fa
Julian Gonggrijp <dev@juliangonggrijp.com>
2018-02-23 16:19:55 +0100

    Add two server-side application test fixtures

--- campbellsoup/conftest.py
+++ campbellsoup/conftest.py
@@ -5,6 +5,8 @@ from string import printable
 
 import pytest
 
+from . import create_application, db, defaults
+
 
 def generate_random_passwords(count=None, mean_length=12, seed=None):
     """ Generate `count` random passwords, or infinitely many if None. """
@@ -21,3 +23,27 @@ def generate_random_passwords(count=None, mean_length=12, seed=None):
 @pytest.fixture(params=generate_random_passwords(15))
 def random_password_fix(request):
     return request.param
+
+
+class UnittestConfig:
+    SECRET_KEY = 'poiuytrewqlkjhgfdsamnbvcxz'
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'  # in-memory
+    SQLALCHEMY_TRACK_MODIFICATIONS = defaults.SQLALCHEMY_TRACK_MODIFICATIONS
+    STATIC_FOLDER = defaults.STATIC_FOLDER
+    STATIC_URL_PATH = defaults.STATIC_URL_PATH
+
+
+@pytest.fixture
+def app_fix():
+    """ Provide an instance of the application with Flask's test_client. """
+    app = create_application(UnittestConfig)
+    app.testing = True
+    return app
+
+
+@pytest.fixture
+def app_db_fix():
+    """ Like app_fix, but with the database fully set up. """
+    app = create_application(UnittestConfig, True)
+    app.testing = True
+    return app, db
