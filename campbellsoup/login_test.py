faf7ddb170fbd12fe6a3ae36d5f31657e00e6a12
Julian Gonggrijp <j.gonggrijp@uu.nl>
2018-03-19 15:34:19 +0100

    Test get_account_by_unicode while we're at it

--- campbellsoup/login_test.py
+++ campbellsoup/login_test.py
@@ -9,6 +9,13 @@ from .conftest_constants import JSON
 from .login import *
 
 
+def test_get_account_by_unicode(account_db_fix):
+    id = account_db_fix.id
+    nonexistent_id = 10000
+    assert get_account_by_unicode(str(id)) == account_db_fix
+    assert get_account_by_unicode(str(nonexistent_id)) == None
+
+
 def test_unauthorized_handler(app_fix):
     @app_fix.route('/test')
     @login_required
